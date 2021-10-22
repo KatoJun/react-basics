f90b0f24bf47ef753150bfaa9728cf0867dd0a31
KatoJun <jun.kato0208@gmail.com>
2021-10-22 11:02:34 +0900

    useEffectまで

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   console.log("最初");
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
 
   const onClickCountUp = () => {
     setNum(num + 1);
@@ -13,6 +14,20 @@ const App = () => {
   const onClickSwitchShowFlag = () => {
     setFaceShowFlag(!faceShowFlag);
   };
+
+  /**
+   * useEffectの第2引数の配列に片酢を指定すると、
+   * その変数の値が変わった時だけuseEffect内の関数が実行される。
+   * 空の配列を指定すると、初回のレンダリング1回だけ関数が実行される。
+   */
+  useEffect(() => {
+    if (num > 0 && num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
 
   return (
     <>
